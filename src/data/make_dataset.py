b9cbf522d8a603c121a436d2703dce32299e2474
antonrydahl <rydahl2610@gmail.com>
2022-01-07 14:38:16 +0100

    Make data implemented

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -4,6 +4,9 @@ import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 
+from datasets import load_dataset
+from transformers import AutoTokenizer
+import torch
 
 @click.command()
 @click.argument('input_filepath', type=click.Path(exists=True))
@@ -14,6 +17,25 @@ def main(input_filepath, output_filepath):
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
+
+    raw_datasets = load_dataset("imdb")
+
+    tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
+
+    def tokenize_function(examples):
+        return tokenizer(examples["text"], padding="max_length", truncation=True)
+
+    tokenized_datasets = raw_datasets.map(tokenize_function, batched=True)
+
+    small_train_dataset = tokenized_datasets["train"].shuffle(seed=42).select(range(1000))
+    small_eval_dataset = tokenized_datasets["test"].shuffle(seed=42).select(range(1000))
+    full_train_dataset = tokenized_datasets["train"]
+    full_eval_dataset = tokenized_datasets["test"]
+
+    torch.save(small_train_dataset,output_filepath+'/train_small.pt')
+    torch.save(small_eval_dataset,output_filepath+'/eval_small.pt')
+    torch.save(full_train_dataset,output_filepath+'/train.pt')
+    torch.save(full_eval_dataset,output_filepath+'/eval.pt')
 
 
 if __name__ == '__main__':
---
+++ src/models/train_model.py
@@ -0,0 +1,53 @@
+from transformers import AutoModelForSequenceClassification
+from torch.utils.data import DataLoader
+from transformers import AdamW, get_scheduler
+
+import torch
+
+output_filepath = '/data/processed'
+small_train_dataset = torch.load(output_filepath+'/train_small.pt')
+small_eval_dataset = torch.load(output_filepath+'/eval_small.pt')
+
+train_dataloader = DataLoader(small_train_dataset, shuffle=True, batch_size=8)
+eval_dataloader = DataLoader(small_eval_dataset, batch_size=8)
+
+model = AutoModelForSequenceClassification.from_pretrained("bert-base-cased", num_labels=2)
+
+optimizer = AdamW(model.parameters(), lr=5e-5)
+
+num_epochs = 3
+num_training_steps = num_epochs * len(train_dataloader)
+lr_scheduler = get_scheduler("linear", optimizer=optimizer, num_warmup_steps=0, num_training_steps=num_training_steps)
+
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+model.to(device)
+
+from tqdm.auto import tqdm
+
+progress_bar = tqdm(range(num_training_steps))
+
+model.train()
+for epoch in range(num_epochs):
+    for batch in train_dataloader:
+        batch = {k: v.to(device) for k, v in batch.items()}
+        outputs = model(**batch)
+        loss = outputs.loss
+        loss.backward()
+
+        optimizer.step()
+        lr_scheduler.step()
+        optimizer.zero_grad()
+        progress_bar.update(1)
+
+metric = load_metric("accuracy")
+model.eval()
+for batch in eval_dataloader:
+    batch = {k: v.to(device) for k, v in batch.items()}
+    with torch.no_grad():
+        outputs = model(**batch)
+
+    logits = outputs.logits
+    predictions = torch.argmax(logits, dim=-1)
+    metric.add_batch(predictions=predictions, references=batch["labels"])
+
+metric.compute()
