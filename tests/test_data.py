df6abb1a6a2ad97d908139f6dc2a6c5ad34e36d3
LauritsFromberg <s174512@student.dtu.dk>
2022-01-14 09:23:26 +0100

    tests

--- tests/test_data.py
+++ tests/test_data.py
@@ -9,3 +9,8 @@ test_set = torch.load(folder + '/eval.pt')
 # Define size of datasets
 N_train = 25000
 N_test = 25000
+
+def test_data_length():
+   assert len(train_set) == N_train, 'Dataset does not have the correct number of samples'
+   assert len(test_set) == N_test, 'Dataset does not have the correct number of samples'
+   
