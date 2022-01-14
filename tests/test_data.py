701df0b68e7e289f5f1bcaee9dd88121c7a6a75f
LauritsFromberg <s174512@student.dtu.dk>
2022-01-14 13:07:07 +0100

    ci

--- tests/test_data.py
+++ tests/test_data.py
@@ -5,7 +5,6 @@ folder = 'C:/Users/Bruger/Documents/MLops/MLOps_Transformers/data/processed'
 train_set = torch.load(folder + '/train.pt')
 test_set = torch.load(folder + '/eval.pt')
 
-
 # Define size of datasets
 N_train = 25000
 N_test = 25000
