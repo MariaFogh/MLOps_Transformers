2a8d8b5d53deaf9bc2024de7ab3e886560986798
antonrydahl <rydahl2610@gmail.com>
2022-01-14 13:18:05 +0100

    Removed sweep temporarily

--- entrypoints.sh
+++ entrypoints.sh
@@ -1,8 +1,9 @@
 # Making data
 python src/data/make_dataset.py
 # Parameter estimation
-NUM=10
-SWEEPID="axhg8mqz"
-wandb agent --count $NUM $SWEEPID
+#NUM=5
+#SWEEPID="axhg8mqz"
+#wandb agent --count $NUM $SWEEPID
+python src/data/train_model.py
 # Validation of model
 python src/data/predict_model.py
