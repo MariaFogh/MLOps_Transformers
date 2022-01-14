2ca7e5c999b987d3be5813fe575fede7ddc2dd43
Enny <ennytao@gmail.com>
2022-01-14 13:14:06 +0100

    Test data

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,11 +1,55 @@
 import torch
+import os
+import pytest
 
-folder = 'C:/Users/Bruger/Documents/MLops/MLOps_Transformers/data/processed'
-
-train_set = torch.load(folder + '/train.pt')
-test_set = torch.load(folder + '/eval.pt')
-
+folder = "data/processed"
 
 # Define size of datasets
-N_train = 25000
-N_test = 25000
+N_train = 1000
+N_test = 1000
+
+
+@pytest.mark.skipif(not os.path.exists(folder), reason="Data files not found")
+def test_data_length():
+    train_data = torch.load(folder + "/train_small.pt")
+    test_data = torch.load(folder + "/eval_small.pt")
+    assert (
+        len(train_data) == N_train
+    ), "Dataset did not have the correct number of samples"
+    assert (
+        len(test_data) == N_test
+    ), "Dataset did not have the correct number of samples"
+
+
+@pytest.mark.skipif(not os.path.exists(folder), reason="Data files not found")
+def test_dataset_features():
+    train_data = torch.load(folder + "/train_small.pt")
+    test_data = torch.load(folder + "/eval_small.pt")
+    assert set(train_data.features.keys()) == set(
+        ["attention_mask", "input_ids", "labels", "token_type_ids"]
+    )
+
+    assert set(test_data.features.keys()) == set(
+        ["attention_mask", "input_ids", "labels", "token_type_ids"]
+    )
+
+
+@pytest.mark.skipif(not os.path.exists(folder), reason="Data files not found")
+def test_dimensions():
+    train_data = torch.load(folder + "/train_small.pt")
+    test_data = torch.load(folder + "/eval_small.pt")
+
+    for dataset in [train_data, test_data]:
+        assert list(dataset["attention_mask"].size()) == [1000, 512]
+        assert list(dataset["input_ids"].size()) == [1000, 512]
+        assert list(dataset["labels"].size()) == [1000]
+        assert list(dataset["token_type_ids"].size()) == [1000, 512]
+
+
+@pytest.mark.skipif(not os.path.exists(folder), reason="Data files not found")
+def test_labels():
+    train_data = torch.load(folder + "/train_small.pt")
+    test_data = torch.load(folder + "/eval_small.pt")
+
+    assert all(i in train_data["labels"] for i in range(2))
+    assert all(i in test_data["labels"] for i in range(2))
